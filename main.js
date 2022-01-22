8a065a23421c156d97b829c1c0b2a813790095c1
Lucas Patrick <lucas.patrick.lsilva@gmail.com>
2022-01-22 11:18:32 -0300

    voltamos

---
+++ main.js
@@ -0,0 +1,30 @@
+// Toggle Menu
+const navigation = document.getElementById("navigation");
+const toggle = document
+  .querySelectorAll("#navigation .toggle")
+  .forEach((el) => {
+    el.addEventListener("click", () => {
+      navigation.classList.toggle("show");
+    });
+  });
+
+// Fechar menu ao clicar
+
+const link = document.querySelectorAll("nav ul li a").forEach((el) => {
+  el.addEventListener("click", () => {
+    navigation.classList.remove("show");
+  });
+});
+
+// Sombra no meu header
+
+let header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', ()=> {
+    if (window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+})
