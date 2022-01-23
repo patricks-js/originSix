c674dcb5effc2b3311069e8133cee1e6d70be38a
Lucas Patrick <lucas.patrick.lsilva@gmail.com>
2022-01-23 09:13:46 -0300

    scroll

--- main.js
+++ main.js
@@ -18,25 +18,45 @@ const link = document.querySelectorAll("nav ul li a").forEach((el) => {
 
 // Sombra no meu header
 
-let header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+let header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', ()=> {
-    if (window.scrollY >= navHeight) {
-        header.classList.add('scroll')
-    } else {
-        header.classList.remove('scroll')
-    }
-})
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= navHeight) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
 
 // Carrousel swiper
-const swiper = new Swiper('.swiper', {
-    slidePerView: 1,
-    loop: true,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+const swiper = new Swiper(".swiper", {
+  slidePerView: 1,
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+// ScrollReveal
+
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `
+#home .image, #home .text, 
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials, 
+#contact .text, #contact .links
+`,
+  { interval: 100 }
+);
